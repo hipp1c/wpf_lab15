193861bb2ecc6a254105a14501a16967dcb2886d
gleba chernavskiy <ppchigleb@gmail.com>
2025-01-30 00:47:26 +0500

    wpf_lab16

--- wpf_lab15/MainWindow.xaml.cs
+++ wpf_lab15/MainWindow.xaml.cs
@@ -16,9 +16,65 @@ namespace wpf_lab15
     /// </summary>
     public partial class MainWindow : Window
     {
+        public List<ToDo> toDoList = new List<ToDo>();
         public MainWindow()
         {
             InitializeComponent();
+
+            descriptionToDo.Text = "Описания нет";
+            dateToDo.SelectedDate = new DateTime(2024, 1, 10);
+
+            toDoList.Add(new ToDo("Приготовить покушать", new DateTime(2024, 1, 15), "Нет описания"));
+            toDoList.Add(new ToDo("Поработать", new DateTime(2024, 1, 20), "Съездить на совещание в Москву"));
+            toDoList.Add(new ToDo("Отдохнуть", new DateTime(2024, 2, 1), "Съездить в отпуск в Сочи"));
+            toDoList.Add(new ToDo("Покормить котика", new DateTime(2024, 2, 2), "Нет описания"));
+            toDoList.Add(new ToDo("Забрать посылку", new DateTime(2024, 2, 21), "Почта на ул. Крауля, 74"));
+            toDoList.Add(new ToDo("Прибраться дома", new DateTime(2024, 1, 29), "Нет описания"));
+            
+            listToDo.ItemsSource = toDoList;
+        }
+
+        private void CheckBox_Click(object sender, RoutedEventArgs e)
+        {
+            if (checkBoxToDo.IsChecked == true)
+            {
+                groupBoxToDo.Visibility = Visibility.Visible;
+                addToDo.Visibility = Visibility.Visible;
+            }
+            else
+            {
+                groupBoxToDo.Visibility = Visibility.Hidden;
+                addToDo.Visibility = Visibility.Hidden;
+            }
+        }
+
+        private void addToDo_Click(object sender, RoutedEventArgs e)
+        {
+            if (titleToDo.Text == null || dateToDo.SelectedDate == null || titleToDo.Text == "") return;
+
+            var todo = new ToDo();
+
+            todo.Name = titleToDo.Text;
+            titleToDo.Text = null;
+
+            todo.Date = dateToDo.SelectedDate.Value;
+            dateToDo.SelectedDate = null;
+
+            todo.Description = descriptionToDo.Text;
+            descriptionToDo.Text = "Описания нет";
+
+            toDoList.Add(todo);
+            listToDo.Items.Refresh();
+        }
+
+        private void removeToDo_Click(object sender, RoutedEventArgs e)
+        {
+            if (listToDo.SelectedItem == null) return;
+
+            var todo = listToDo.SelectedItem as ToDo;
+
+            toDoList.Remove(todo);
+            listToDo.Items.Refresh();
         }
     }
 }
---
+++ wpf_lab15/ToDo.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace wpf_lab15
+{
+    public class ToDo
+    {
+        public string? Name { get; set; }
+        public DateTime Date { get; set; }
+        public string? Description { get; set; }
+
+        public ToDo() { }
+
+        public ToDo(string? name, DateTime date, string? description)
+        {
+            Name = name;
+            Date = date;
+            Description = description;
+        }
+    }
+}
